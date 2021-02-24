765e9cc5554e776a434ff8651106a739fb061858
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-24 12:15:35 +0100

    [fix] fix du passage a goal

--- app/src/main/java/com/example/objectifsportif/Model/AppDatabase.java
+++ app/src/main/java/com/example/objectifsportif/Model/AppDatabase.java
@@ -3,7 +3,7 @@ package com.example.objectifsportif.model;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities={Activite.class,Objectif.class,Sport.class},version = 1)
+@Database(entities={Activite.class, Goal.class,Sport.class},version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
 }
